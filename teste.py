d12bd4b2821b5fc2f006dda82b0f5d7e963fd426
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-02 20:17:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.write('Demanda mensal de carne moída para uma família de 5 pessoas.')
+st.write('A família usa carne moída para fazer vários pratos, inclusive hamburguer.')
+st.write('Dada uma situação, que inclui preços de vários produtos, renda, e preferências , a família decide quanto de carne moída precisa (ou pode) comprar.')
