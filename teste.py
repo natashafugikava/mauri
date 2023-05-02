a0ca3d882450a1daddf6fa05e49e2979367ccfea
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-02 20:32:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,4 +5,4 @@ st.write('A família usa carne moída para fazer vários pratos, inclusive hamb�
 st.write('Dada uma situação, que inclui preços de vários produtos, renda, e preferências , a família decide quanto de carne moída precisa (ou pode) comprar.')
 
 st.slider('Preço da carne moída (Kg)', 10.00, 50.00, 30.00)
-st.slider('Preço do pão de hambúrguer (unidade)', 5.00, 10,00, 7.50)
+st.slider('Preço do pão de hambúrguer (unidade)', 5.00, 10.00, 7.50)
