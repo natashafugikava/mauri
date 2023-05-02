79849762813dc235f921ef6fadbd38bf736d531a
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-02 20:26:33 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,4 +4,4 @@ st.write('Demanda mensal de carne moída para uma família de 5 pessoas.')
 st.write('A família usa carne moída para fazer vários pratos, inclusive hambúrguer.')
 st.write('Dada uma situação, que inclui preços de vários produtos, renda, e preferências , a família decide quanto de carne moída precisa (ou pode) comprar.')
 
-st.slider(label='Preço da carne moída (Kg)', min_value=10, max_value=50, value=30, step=0.01)
+st.slider('Preço da carne moída (Kg)', 10, 50, 30, 0.01)
