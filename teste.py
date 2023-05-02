b3a67ff2986b5b389d951ed237e5935cde4dc844
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-02 20:39:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,4 +7,4 @@ st.write('Dada uma situação, que inclui preços de vários produtos, renda, e 
 st.slider('Preço da carne moída (Kg)', 10.00, 50.00, 30.00)
 st.slider('Preço do pão de hambúrguer (unidade)', 5.00, 10.00, 7.50)
 st.slider('Preço do ketchup (500g)', 5.00, 20.00, 10.00)
-st.slider('Preço da muçarela (Kg)', 20.00, 100.00, 45.00)
+st.slider('Preço da mussarela (Kg)', 20.00, 100.00, 45.00)
