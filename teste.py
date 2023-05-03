1f52d61758ff24da6f1823e4ad5b961c691aaf3c
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-03 10:32:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -30,4 +30,4 @@ consumo = teto/carne + teto/lanche
 with col2:
   st.write('Espaço reservado para o gráfico.')
   st.write(f'Gasto total com carne e sanduíches: R$ {2*teto:.2f}')
-  st.write(f'Consumo total de carne para outros fins: R$ {teto/carne:.2f}')
+  st.write(f'Consumo total de carne para outros fins: {teto/carne:.2f} Kg')
