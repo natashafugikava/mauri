bab219d417a3c2d55f503d8e945082dfd4d3ae8b
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-03 10:43:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -32,4 +32,5 @@ with col2:
   st.write(f'Gasto total com carne e sanduíches: R$ {2*teto:.2f}')
   st.write(f'Consumo total de carne para outros fins: {teto/carne:.2f} Kg')
   st.write(f'Consumo total de carne com sanduíches: {teto/lanche*0.2:.2f} Kg')
-  st.write(f'Consumo total de sauduíches mensais: {teto/lanche:.2f}')
+  st.write(f'Consumo total de sanduíches mensais: {teto/lanche:.2f} unidades')
+  st.write(f'Consumo total de sanduíches mensais por pessoa: {teto/lanche/5:.2f} unidades')
