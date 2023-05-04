c3151a0c2b5ee8045f5ce73c23255acae176084e
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-04 20:05:36 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -6,4 +6,5 @@ import streamlit as st
 
 st.header('Projeto Integrador I')
 df = pd.read_parquet('fatec.parquet')
+df = df[['ad_id', 'id_type', 'latitude', 'longitude', 'datetime_local']]
 st.write(df)
