91d206ac7fe63b29684e28b813aa7c774f66907e
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-04 20:42:08 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -5,10 +5,5 @@ import hvplot.pandas
 import streamlit as st
 
 st.header('Projeto Integrador I')
-df = pd.read_parquet('fatec.parquet')
-df = df[['ad_id', 'id_type', 'latitude', 'longitude', 'datetime_local']]
+df = pd.read_csv('movies.csv')
 st.write(df)
-
-st.map(df, zoom=3)
-#gdf = gpd.GeoDataFrame(df, geometry = gpd.points_from_xy(df.longitude, df.latitude), crs = 4326)
-#st.write(gdf.plot(column='ad_id', markersize=5))
