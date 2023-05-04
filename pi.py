344e5ef27eca3ae4e374ca21f780d5de0cd37ba0
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-04 20:21:05 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -9,5 +9,6 @@ df = pd.read_parquet('fatec.parquet')
 df = df[['ad_id', 'id_type', 'latitude', 'longitude', 'datetime_local']]
 st.write(df)
 
-gdf = gpd.GeoDataFrame(df, geometry = gpd.points_from_xy(df.longitude, df.latitude), crs = 4326)
-st.write(gdf.plot(column='ad_id', markersize=5))
+st.map(df)
+#gdf = gpd.GeoDataFrame(df, geometry = gpd.points_from_xy(df.longitude, df.latitude), crs = 4326)
+#st.write(gdf.plot(column='ad_id', markersize=5))
